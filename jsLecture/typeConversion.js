022ace47a0a70dcc4be9241d7fec54a3bfca2c2c
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-09-09 14:02:09 +0000

    ...

--- jsLecture/typeConversion.js
+++ jsLecture/typeConversion.js
@@ -1,20 +1,27 @@
-let num = 85;
+// let num = 85;
 
-// console.log(num);
-// console.log(typeof num); 
-// // OR / "both are same"
-// console.log(typeof(num));
+// // console.log(num);
+// // console.log(typeof num); 
+// // // OR / "both are same"
+// // console.log(typeof(num));
 
-let numString = String(num);
-console.log(numString);
-console.log(typeof numString);
+// let numString = String(num);
+// console.log(numString);
+// console.log(typeof numString);
 
-let isLoggedIn = "Prince";
-// console.log(isLoggedIn);
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// let isLoggedIn = "Prince";
+// // console.log(isLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 
 
+// ******************************OPERATERS*****************************
+
+let num = 100;
+++num
+// console.log(num);/
+console.log(num);
+// console.log(num)
 
