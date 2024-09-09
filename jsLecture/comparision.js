716e5a5123941c31a6aa93af5af461f7803684c3
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-09-09 14:07:21 +0000

    operaters comparision

---
+++ jsLecture/comparision.js
@@ -0,0 +1,22 @@
+// comparision operaters 
+
+// console.log(9 >0);
+// console.log(9 < 10);
+// console.log(9 >=0);
+// console.log(9 <=0);
+
+
+// console.log(null > 0);
+// console.log(null < 0);
+// below both case null treated as 0 so that is returns the true value.
+// console.log(null >= 0);
+// console.log(null <=0);
+
+// **************************** undefined comaparision
+console.log(undefined > 0);
+console.log(undefined < 0);
+// below both case null treated as 0 so that is returns the true value.
+console.log(undefined >= 0);
+console.log(undefined <=0);
+
+
