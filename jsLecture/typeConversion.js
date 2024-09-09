08c86ecedbf868453a72163ba6d19350d0bd7f8d
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-09-09 13:40:25 +0000

    today i revise type conversions

---
+++ jsLecture/typeConversion.js
@@ -0,0 +1,20 @@
+let num = 85;
+
+// console.log(num);
+// console.log(typeof num); 
+// // OR / "both are same"
+// console.log(typeof(num));
+
+let numString = String(num);
+console.log(numString);
+console.log(typeof numString);
+
+let isLoggedIn = "Prince";
+// console.log(isLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+
+
