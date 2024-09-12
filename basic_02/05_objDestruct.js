c6a0a6b412aed99aaa08dca796a2a63301e5e1c1
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-09-12 10:17:22 +0000

    destructuring of onject

---
+++ basic_02/05_objDestruct.js
@@ -0,0 +1,13 @@
+// In this lecture we learn aboute object destructring, JSON, and APIs
+
+const rectangle = {
+    height: 10,
+    width:30,
+    color:"red",
+    radius:4
+}
+
+//we used destructuring for easy to access object properties
+
+const {height, width,color, radius } = rectangle;
+console.log(color);
