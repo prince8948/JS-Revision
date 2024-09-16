0957b71a6653553e80c065e1f269e2726e38dce7
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-09-16 15:26:36 +0000

    whiles working

---
+++ 05_iterations/02_whileloops.js
@@ -0,0 +1,13 @@
+/* A while statement executes its statements as long as a specified condition evaluates to true. A while statement
+ looks as follows:
+    while (condition)
+    statement
+*/
+
+let i = 1; //this is initialization of variables
+while(i<=9){ //condition or terminator
+    console.log(i)
+    i++ // increaments of variable so loops runs repeatedly
+}
+
+//same as for loops we can break the loops here 
