e9e84214c9ea7c3da5d1d839e1956c364f2cabf0
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-09-17 05:24:06 +0000

    do while working

--- 05_iterations/02_whileloops.js
+++ 05_iterations/02_whileloops.js
@@ -10,4 +10,52 @@ while(i<=9){ //condition or terminator
     i++ // increaments of variable so loops runs repeatedly
 }
 
-//same as for loops we can break the loops here 
+//same as for loops we can break the loops 
+//Ex -->
+
+let k = 1;
+while(k<=6){
+    if(k==3){
+        break;
+    }
+    console.log(k);
+    k= k+1;
+}
+
+// same if you want to pass this conditions
+let x =1;
+while(x<6){
+    if(x==3){
+        console.log("pass this");
+        x++ // Increment before continue to avoid infinite loop
+        continue;
+    }
+    // console.log(x);
+    x++;
+}
+
+
+
+// Lets learn about do while loops-> in do while loops at ones it will executed because it return the value then check the condition
+//EX   --->
+
+let l = 1;
+do {
+    console.log("table of 2's", 2*l)
+    l++
+} while (l<=10);
+
+// this code returns the 22 because it return the then do increament and check condition. Condition is false so it exits the loops
+console.log("for dowgile loops outputs - >");
+
+let n = 1;
+do {
+    if(n == 4){
+        break;
+    }
+    console.log("table of 2's", 2*n)
+    n++
+} while (n<=10);
+
+
+
