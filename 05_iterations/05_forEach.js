55ee0d868137e27e721fb8bb8e89a9ff758c5e02
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-09-18 04:45:56 +0000

    practice some array questions

--- 05_iterations/05_forEach.js
+++ 05_iterations/05_forEach.js
@@ -5,25 +5,95 @@
 
 const fruits = ['mango', 'apple', 'banana', 'graps','kivi','orange']
 fruits.forEach(function(items){
-    console.log(items)
+    // console.log(items)
 })
 // using arrow funtions 
 fruits.forEach((val)=>{
-    console.log("arrow ->",val);
+    // console.log("arrow ->",val);
 })
 
 // using function reference 
 function printFruitName(name){
-    console.log(name);
+    // console.log(name);
 }
 fruits.forEach((printFruitName))
 
 
 //forEach has three parameters index, items, array list 
 fruits.forEach((index,val,arrayList)=>{
-    console.log(index," - ", val, "list - ", arrayList);
+    // console.log(index," - ", val, "list - ", arrayList);
 })
 
 
 
 // how to used forEach with objects ------------------------------------------
+const myobj = [{
+    IN: 'INDIA',
+    AFG: 'AFGANISTAN',
+    AUS:'AUSTRALIA',
+    USA: 'UNITED STATES OF KINNGDOM',
+    CH:'CHINA',
+    NZ:'NEWZILAND',
+    SL:'SGRILANKA'
+}]
+
+myobj.forEach((states)=>{
+    // console.log(states);
+    // console.log(states.AFG);
+})
+
+// create another array with object 
+const std = [
+    user1 ={
+        name:'raj',
+        age:'19',
+        city:'lko',
+        course:'MCA',
+        skills:['html','css', 'sql', 'js'],
+        isLoggedIn:true
+    },    
+    user5 ={
+        name:'rajesh',
+        age:'18',
+        city:'bbk',
+        course:'MCA',
+        skills:['html','java', 'sql', 'js'],
+        isLoggedIn:true
+    }, 
+    user2 ={
+        name:'rahul',
+        age:'20',
+        city:'CNB',
+        course:'BCA',
+        skills:['java', 'sql', 'js'],
+        isLoggedIn:1
+    },
+    user3 ={
+        name:'raja',
+        age:'22',
+        city:'GD',
+        course:'BSc',
+        skills:['sql', 'js'],
+        isLoggedIn:true
+    } ,
+    user4 ={
+        name:'prince',
+        age:'24',
+        city:'lko',
+        course:'MCA',
+        skills:['js'],
+        isLoggedIn:false
+    }  
+]
+let arr=[];
+std.forEach((stdDetails)=>{
+    if(stdDetails.isLoggedIn == true){
+        arr = stdDetails.name;
+        // if(stdDetails.skills.length>2){
+        //     console.log(`${stdDetails.name} : ${stdDetails.course}`)
+        // }
+        console.log(arr);   
+    }
+})
+console.log(arr.length);
+
