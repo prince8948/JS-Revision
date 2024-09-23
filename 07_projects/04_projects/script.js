80b6d87711239019b5a76af41e79f68128c570b3
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-09-23 17:04:36 +0000

    changes on BMI Guide

--- 07_projects/04_projects/script.js
+++ 07_projects/04_projects/script.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', function(e){
     const userHeight = parseInt(document.querySelector('#userheight').value);
     const userWeight = parseInt(document.querySelector('#userWeight').value);
     const result = document.querySelector('#Result');
-    
+    let BMI;
     if(userHeight=='' || userHeight<0 || isNaN(userHeight)){
         result.innerHTML = `Please Enter Valid Height ${userHeight}`;
     }else if(userWeight=='' || userWeight<0 || isNaN(userWeight)){
@@ -14,8 +14,16 @@ form.addEventListener('submit', function(e){
         // console.log(userHeight);
     }
     else{
-        const BMI = (userWeight/((userHeight*userHeight)/10000)).toFixed(2);
-        result.innerHTML = `<span>You Body Index- ${BMI} </span>`;
+        BMI = (userWeight/((userHeight*userHeight)/10000)).toFixed(2);
+        result.innerHTML = `<span>You Body Mass Index- ${BMI} </span>`;
+    }
+    if(BMI > 0 && BMI < 18.6){
+        document.querySelector('.bmiGuide').innerHTML = `Under Weight (<18.6)= ${BMI}`
+    }else if(BMI>=18.6 && BMI<24.9){
+        document.querySelector('.bmiGuide').innerHTML = `Normal Weight (18.6 to 24.9) =  ${BMI}`
+    }
+    else{
+        document.querySelector('.bmiGuide').innerHTML = `Over Weight (>24.9) =  ${BMI}`
     }
     
 });
