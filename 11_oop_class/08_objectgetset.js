e1de4eb8c5ea819aa2c8cf201257beddb62a90df
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-10-10 14:49:50 +0000

    getter setter done now

--- 11_oop_class/08_objectgetset.js
+++ 11_oop_class/08_objectgetset.js
@@ -1,19 +1,33 @@
 const User={
     _name:'Prince',
-    userId:'121',
-    userEmail:'prince@121.com',
+    _userId:'121',
+    _userEmail:'prince@121.com',
     //here we create getter setter for name 
     get name(){
         return this._name.toUpperCase();
     },
     set name(userName){
         this._name = userName;
-    }
+    },
     //here creates for user id
-
-
-
+    get userId(){
+        return this._userId;
+    },
+    set userId(id){
+        this._userId = id
+    },
     //here creates for userEmails
-    
+    get userEmail(){
+        return this._userEmail
+    },
+    set userEmail(email){
+        this._userEmail = email
+    } 
+
 }
 console.log(User.name);
+console.log(User.userId);
+console.log(User.userEmail);
+// let do logs in one line 
+console.log(`My name : ${User.name} & Emp Id : ${User.userId} & email : ${User.userEmail}`);
+
