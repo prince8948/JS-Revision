a96bda1891122ccfa0e8888a927ad1295cf5ce3c
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-10-10 14:35:10 +0000

    how to getter setter create with objects

--- 11_oop_class/08_objectgetset.js
+++ 11_oop_class/08_objectgetset.js
@@ -1,6 +1,19 @@
 const User={
-    name:'Prince',
+    _name:'Prince',
     userId:'121',
     userEmail:'prince@121.com',
+    //here we create getter setter for name 
+    get name(){
+        return this._name.toUpperCase();
+    },
+    set name(userName){
+        this._name = userName;
+    }
+    //here creates for user id
+
+
+
+    //here creates for userEmails
     
 }
+console.log(User.name);
