cc7f521010d1d84013bc08dd67051835952707e3
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-10-13 13:44:31 +0000

    learn about scope in js And details in laxical scoping

---
+++ 12_laxicalscopClousors/laxical.js
@@ -0,0 +1,13 @@
+/* Lexical Scope:- JavaScript uses lexical scoping, which means that a variable's scope is determined by 
+its location in the source code when the code is written. When a function is called, it references the variables 
+that are in the same scope as it was declared, not where the function is called from.
+*/
+function outer(){
+    let userDetailes = 'Hello my name is prince prajapati'
+    function inner(){
+        userDetailes = 'details are changed'
+        console.log(userDetailes)
+    }inner()
+    console.log(userDetailes);
+    
+}outer()
