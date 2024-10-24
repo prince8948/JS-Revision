f48a9f80b0c02432b6ea5e15027259fde6f88439
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-10-24 17:22:43 +0000

    finally i complete my weather app and it properily work with wearther app api

--- 07_projects/08weatherApp/script.js
+++ 07_projects/08weatherApp/script.js
@@ -1,6 +1,22 @@
 const apiKey = 'fd065ae231b2421147abc9dbaecc9f83'
-const apirUrl = ''
+const apirUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
+
+const inputBox = document.querySelector('.input')
+const btn = document.querySelector('.btn')
+
+
+
 async function fetchWeatherData(city) {
-    const res = await fetch(apirUrl +   `${apiKey}`)
-    const data = res.json()
+    const res = await fetch(apirUrl + city+ `&appid=${apiKey}`);
+    const data = await res.json();
+    
+    document.querySelector('.temp').innerHTML=Math.round(data.main.temp) + '°C';
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.h-per').innerHTML = data.main.humidity+'%';
+    document.querySelector('.w-per').innerHTML = data.wind.speed +'Km/h';
+    console.log(data);
+    
 }
+btn.addEventListener('click',()=>{
+    fetchWeatherData(inputBox.value)
+})
