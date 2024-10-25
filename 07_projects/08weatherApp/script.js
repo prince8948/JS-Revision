552c3996f2b04b4d3eda2812eac9deeb7e097537
Prince Prajapati <78802843+prince8948@users.noreply.github.com>
2024-10-25 05:52:46 +0000

    In my weather app i apply the condition accordin to the condition weather icons will changes

--- 07_projects/08weatherApp/script.js
+++ 07_projects/08weatherApp/script.js
@@ -3,20 +3,34 @@ const apirUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=
 
 const inputBox = document.querySelector('.input')
 const btn = document.querySelector('.btn')
+const weatherImg = document.querySelector('.weather-icon')
 
 
-
-async function fetchWeatherData(city) {
+async function fetchWeatherData(city='lucknow') {
     const res = await fetch(apirUrl + city+ `&appid=${apiKey}`);
     const data = await res.json();
     
     document.querySelector('.temp').innerHTML=Math.round(data.main.temp) + '°C';
     document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.h-per').innerHTML = data.main.humidity+'%';
+    document.querySelector('.h-per').innerHTML = data.main.humidity + '%';
     document.querySelector('.w-per').innerHTML = data.wind.speed +'Km/h';
-    console.log(data);
-    
+    if(data.weather[0].main =='Clouds'){
+        weatherImg.src = "images/cloudes.png"
+    }
+    else if(data.weather[0].main == 'Snow'){
+        weatherImg.src = 'images/snow.png'
+    }
+    else if(data.weather[0].main == 'Clear'){
+        weatherImg.src = 'images/sun.png'
+    }
+    else if(data.weather[0].main == 'Fog'){
+        weatherImg.src = 'images/fog.png'
+    }
+    else if(data.weather[0].main == 'Rain'){
+        weatherImg.src = 'images/rainy.png'
+    }
 }
 btn.addEventListener('click',()=>{
+    
     fetchWeatherData(inputBox.value)
 })
